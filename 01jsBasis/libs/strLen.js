8b29943dd97a8ba5a022cbea8ffe232a4006f3fd
tony <tony.liu@e3-expo.com>
2017-08-14 14:53:27 +0800

    添加jsBasis代码

---
+++ 01jsBasis/libs/strLen.js
@@ -0,0 +1,19 @@
+var strLen = (function() {
+    var trim = function(chars){
+        return (chars || "").replace( /^(\s|\u00A0)+|(\s|\u00A0)+$/g, "" );
+    }
+
+    return function(_str, _model) {
+        _str = trim(_str),
+            _model = _model || "Ch";	//默认是中文
+        var _strLen = _str.length;	//获取字符长度
+        if(_strLen == 0){			//如果字符为0直接返回
+            return 0;
+        }
+        else{
+            var chinese = _str.match(/[\u4e00-\u9fa5]/g);				//匹配中文
+//判断是什么模式
+            return _strLen + (chinese && _model == "Ch" ? chinese.length: 0);
+        }
+    };
+})();
